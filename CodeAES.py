5075a8a9a1dc89b4fc485332c595deeb75cb6cf7
ZiyadAlajmi <54519322+ZiyadAlajmi@users.noreply.github.com>
2019-10-18 14:23:47 -0400

    Update CodeAES.py
    Encrypts each ROI and decrypts each ROI

--- CodeAES.py
+++ CodeAES.py
@@ -5,37 +5,40 @@ from PIL import Image
 import numpy
 import cv2
 
-def encrypt(encryptor, coordinates):
+def encrypt(encryptor, coordinates, image):
     ###################################################################
-    image = cv2.imread('TestImage.jpg')
-
     ROI_number = 0
     for c in coordinates:
-        cv2.rectangle(image, (400, 400), (600, 200), (0, 0, 255), 1)
+        
         x,y,w,h = c
         ROI = image[y:y+h, x:x+w]
         img_bytes = numpy.ascontiguousarray(ROI, dtype=None)
         
+        cv2.imshow("Image", ROI) #Just shows ROI
+        cv2.waitKey(0) #For showing ROI
+        
         encData = encryptor.encrypt(img_bytes) #Encrypted data
         
-        encFile = open("Encrypted.enc", "wb") #creates encrypted file in directory
+        encFile = open("Encrypted" + str(c) + ".enc", "wb") #creates encrypted file in directory
         encFile.write(encData)
         encFile.close()
         
         ROI_number += 1
     #################################################################
 
-def decrypt(decryptor):
+def decrypt(decryptor, coordinates):
+    
+    ROI_number = 0
+    for c in coordinates:
+        encFile2 = open("Encrypted" + str(c) + ".enc", "rb") #Opens encrypted file created earlier
+        encData2 = encFile2.read()
+        encFile2.close()
 
-    encFile2 = open("Encrypted.enc", "rb") #Opens encrypted file created earlier
-    encData2 = encFile2.read()
-    encFile2.close()
+        decData = decryptor.decrypt(encData2) #Decryption line
 
-    decData = decryptor.decrypt(encData2) #Decryption line
-
-    output_file = open("NewOutput.jpeg", "wb") #Creates decrypted file in directory
-    output_file.write(decData)
-    output_file.close()
+        output_file = open("Decrypted_ROI" + str(c) + ".jpg", "wb") #Creates decrypted file in directory
+        output_file.write(decData)
+        output_file.close()
 
 if __name__=="__main__":    
     
@@ -43,10 +46,11 @@ if __name__=="__main__":
     IV = Random.new().read(AES.block_size) #Initialization vector
     mode = AES.MODE_CFB #Sets encryption mode to CFB mode; CFB is great in avoiding the hassle of padding
     
-    coordinates = [(200, 300, 400, 250), (500, 800, 830, 612)]
+    image = cv2.imread('TestImage.jpg')
+    coordinates = [(300, 300, 800, 600), (500, 800, 830, 612), (400, 300, 700, 550), (400, 900, 800, 200)]
     
     encryptor = AES.new(key, mode, IV) # Encryptor
     decryptor = AES.new(key, mode, IV) # Decryptor
 
-    encrypt(encryptor, coordinates) #Calls encryption function
-    #decrypt(decryptor) #Calls decryption function
+    encrypt(encryptor, coordinates, image) #Calls encryption function
+    decrypt(decryptor, coordinates) #Calls decryption function
