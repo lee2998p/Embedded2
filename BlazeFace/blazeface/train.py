0f623a881f88f064fdfd6409f1826f18f91d6ffb
ishaghodgaonkar <ighodgao@purdue.edu>
2020-02-05 01:18:29 -0500

    made changes to blazeface

---
+++ BlazeFace/blazeface/train.py
@@ -0,0 +1,199 @@
+from data import wider_face
+from blazeface.data import HOME
+from wider_face import WIDERDetection
+from wider_face import WIDER_ROOT
+from wider_face import WIDER_ROOT
+from layers import MultiBoxLoss
+from utils.augmentations import SSDAugmentation
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torch
+import argparse
+from model import *
+import numpy as np
+import time
+
+
+parser = argparse.ArgumentParser(
+    description='BlazeFace Training With Pytorch')
+train_set = parser.add_mutually_exclusive_group()
+parser.add_argument('--dataset', default='WIDER', choices=['VOC', 'COCO', 'WIDER'],
+                    type=str)
+parser.add_argument('--dataset_root', default=HOME + '/PyTorch_BlazeFace/WIDER',
+                    help='Dataset root directory path')
+# parser.add_argument('--basenet', default='vgg16_reducedfc.pth',
+#                     help='Pretrained base model')
+parser.add_argument('--batch_size', default=64, type=int,
+                    help='Batch size for training')
+parser.add_argument('--resume', default=None, type=str,
+                    help='Checkpoint state_dict file to resume training from')
+parser.add_argument('--start_iter', default=0, type=int,
+                    help='Resume training at this iter')
+parser.add_argument('--num_workers', default=4, type=int,
+                    help='Number of workers used in dataloading')
+parser.add_argument('--cuda', default=False,
+                    help='Use CUDA to train model')
+parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float,
+                    help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float,
+                    help='Momentum value for optim')
+parser.add_argument('--weight_decay', default=5e-4, type=float,
+                    help='Weight decay for SGD')
+parser.add_argument('--gamma', default=0.1, type=float,
+                    help='Gamma update for SGD')
+parser.add_argument('--save_folder', default='weights/',
+                    help='Directory for saving checkpoint models')
+args = parser.parse_args()
+
+if torch.cuda.is_available():
+    if args.cuda:
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    if not args.cuda:
+        print("WARNING: It looks like you have a CUDA device, but aren't " +
+              "using CUDA.\nRun with --cuda for optimal training speed.")
+        torch.set_default_tensor_type('torch.FloatTensor')
+
+def train():
+
+    blazeface = BlazeFace()
+
+    net = blazeface
+
+    if args.dataset == 'WIDER':
+        cfg = wider_face
+        dataset = WIDERDetection(root=WIDER_ROOT,
+                                 transform=SSDAugmentation(cfg['min_dim'],
+                                                           ))
+    if args.resume:
+        print('Resuming training, loading {}...'.format(args.resume))
+        net.load_weights(args.resume)
+
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum,
+                          weight_decay=args.weight_decay)
+
+    criterion = MultiBoxLoss(cfg['num_classes'], 0.5, True, 0, True, 3, 0.5,
+                             False, args.cuda)
+
+    net.train()
+
+    loc_loss = 0
+    conf_loss = 0
+    epoch = 0
+    print('Loading the dataset...')
+
+    epoch_size = len(dataset) // args.batch_size
+    print('Training BlazeFace on:', dataset.name)
+    print('Using the specified args:')
+    print(args)
+
+    data_loader = data.DataLoader(dataset, args.batch_size,
+                                  num_workers=args.num_workers,
+                                  shuffle=False, collate_fn=detection_collate,
+                                  pin_memory=True)
+
+
+    batch_iterator = iter(data_loader)
+    for iteration in range(args.start_iter, cfg['max_iter']):
+        if iteration in cfg['lr_steps']:
+            step_index += 1
+            adjust_learning_rate(optimizer, args.gamma, step_index)
+
+        # load train data
+        images, targets = next(batch_iterator)
+
+        if args.cuda:
+            images = Variable(images.cuda())
+            targets = [Variable(ann.cuda(), volatile=True) for ann in targets]
+        else:
+            images = Variable(images)
+            targets = [Variable(ann, volatile=True) for ann in targets]
+            # forward
+        t0 = time.time()
+        print(images[0].shape)
+        out = net(images)
+        print("forward prop done")
+        # backprop
+        optimizer.zero_grad()
+
+        loss_l, loss_c = criterion(out, targets)
+        print(loss_l)
+        print(loss_c)
+
+        loss = loss_l + loss_c
+        loss.backward()
+
+        optimizer.step()
+        t1 = time.time()
+        # loc_loss += loss_l.data[0]
+        # conf_loss += loss_c.data[0]
+
+        loc_loss += loss_l.data
+        conf_loss += loss_c.data
+        print(loc_loss)
+        print(conf_loss)
+
+        if iteration % 1 == 0:
+            print('timer: %.4f sec.' % (t1 - t0))
+            print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.data), end=' ')
+
+        if iteration != 0 and iteration % 1 == 0:
+                    print('here')
+                    print('Saving state, iter:', iteration)
+                    torch.save(net.state_dict(), 'weights/ssd300_WIDER_' +
+                               repr(iteration) + '.pth')
+    torch.save(net.state_dict(),
+                       args.save_folder + '' + args.dataset + '.pth')
+
+
+def adjust_learning_rate(optimizer, gamma, step):
+    """Sets the learning rate to the initial LR decayed by 10 at every
+        specified step
+    # Adapted from PyTorch Imagenet example:
+    # https://github.com/pytorch/examples/blob/master/imagenet/main.py
+    """
+    lr = args.lr * (gamma ** (step))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def multibox(vgg, extra_layers, cfg, num_classes):
+    loc_layers = []
+    conf_layers = []
+    vgg_source = [21, -2]
+    for k, v in enumerate(vgg_source):
+        loc_layers += [nn.Conv2d(vgg[v].out_channels,
+                                 cfg[k] * 4, kernel_size=3, padding=1)]
+        conf_layers += [nn.Conv2d(vgg[v].out_channels,
+                        cfg[k] * num_classes, kernel_size=3, padding=1)]
+    for k, v in enumerate(extra_layers[1::2], 2):
+        loc_layers += [nn.Conv2d(v.out_channels, cfg[k]
+                                 * 4, kernel_size=3, padding=1)]
+        conf_layers += [nn.Conv2d(v.out_channels, cfg[k]
+                                  * num_classes, kernel_size=3, padding=1)]
+    return vgg, extra_layers, (loc_layers, conf_layers)
+
+def detection_collate(batch):
+    """Custom collate fn for dealing with batches of images that have a different
+    number of associated object annotations (bounding boxes).
+    Arguments:
+        batch: (tuple) A tuple of tensor images and lists of annotations
+    Return:
+        A tuple containing:
+            1) (tensor) batch of images stacked on their 0 dim
+            2) (list of tensors) annotations for a given image are stacked on 0 dim
+    """
+    targets = []
+    imgs = []
+    for _, sample in enumerate(batch):
+        for _, tup in enumerate(sample):
+            if torch.is_tensor(tup):
+                imgs.append(tup)
+            elif isinstance(tup, type(np.empty(0))):
+                annos = torch.from_numpy(tup).float()
+                targets.append(annos)
+
+    return (torch.stack(imgs, 0), targets)
+
+train()
---
+++ BlazeFace/blazeface/wider_face.py
@@ -0,0 +1,216 @@
+from data import HOME
+import os
+import os.path as osp
+import sys
+import torch
+import torch.utils.data as data
+import cv2
+import numpy as np
+
+if sys.version_info[0] == 2:
+    import xml.etree.cElementTree as ET
+else:
+    import xml.etree.ElementTree as ET
+
+
+
+WIDER_CLASSES = ('face', )
+
+WIDER_ROOT = osp.join(HOME, "/PyTorch_BlazeFace/")
+
+class WIDERAnnotationTransform(object):
+
+    """Transforms a VOC annotation into a Tensor of bbox coords and label index
+    Initilized with a dictionary lookup of classnames to indexes
+    Arguments:
+        class_to_ind (dict, optional): dictionary lookup of classnames -> indexes
+            (default: alphabetic indexing of VOC's 20 classes)
+        keep_difficult (bool, optional): keep difficult instances or not
+            (default: False)
+        height (int): height
+        width (int): width
+    """
+
+    def __init__(self, class_to_ind=None, keep_difficult=False):
+        self.class_to_ind = class_to_ind or dict(
+            zip(WIDER_CLASSES, range(len(WIDER_CLASSES))))
+        self.keep_difficult = keep_difficult
+
+    def __call__(self, target, width, height):
+        """
+        Arguments:
+            target (annotation) : the target annotation to be made usable
+                will be an ET.Element
+        Returns:
+            a list containing lists of bounding boxes  [bbox coords, class name]
+        """
+        res = []
+        for obj in target.iter('object'):
+            difficult = int(obj.find('difficult').text) == 1
+            if not self.keep_difficult and difficult:
+                continue
+            name = obj.find('name').text.lower().strip()
+            bbox = obj.find('bndbox')
+
+            pts = ['xmin', 'ymin', 'xmax', 'ymax']
+            bndbox = []
+            for i, pt in enumerate(pts):
+                cur_pt = int(bbox.find(pt).text)
+                cur_pt = cur_pt / width if i % 2 == 0 else cur_pt / height
+
+                bndbox.append(cur_pt)
+            label_idx = self.class_to_ind[name]
+            bndbox.append(label_idx)
+            #look here for potential problems
+            #from voc0712.
+            # x1 = bndbox[0]
+            # y1 = bndbox[1]
+            # width = bndbox[2]
+            # height = bndbox[3]
+            #
+            # bndbox[0] = x1
+            # bndbox[1] = x1 + width
+            # bndbox[2] = y1
+            # bndbox[3] = y1 + height
+
+            res += [bndbox]  # [xmin, ymin, xmax, ymax, label_ind]
+            #original
+
+
+            #res = np.vstack((res, bndbox))  # [xmin, ymin, xmax, ymax, label_ind]
+        return res
+
+
+class WIDERDetection(data.Dataset):
+
+    """VOC Detection Dataset Object
+    input is image, target is annotation
+    Arguments:
+        root (string): filepath to WIDER folder
+        target_transform (callable, optional): transformation to perform on the
+            target `annotation`
+            (eg: take in caption string, return tensor of word indices)
+    """
+
+    def __init__(self, root, image_set='wider_train', transform = None, target_transform=WIDERAnnotationTransform(), dataset_name = "WIDER_FACE"):
+        self.root = root
+        self.image_set = image_set
+        self.transform = transform
+        self.target_transform = target_transform
+        self.name = dataset_name
+        self._annopath = os.path.join('/Users/ishaghodgaonkar/PyTorch_BlazeFace/', 'WIDER/annotations', '%s')
+        if image_set == 'wider_train':
+            print("TRAINING DATASET")
+            self._imgpath = os.path.join('/Users/ishaghodgaonkar/PyTorch_BlazeFace/', 'WIDER/WIDER_train/images', '%s')
+        else:
+            print("TESTING DATASET")
+            self._imgpath = os.path.join('/Users/ishaghodgaonkar/PyTorch_BlazeFace/',  'WIDER/WIDER_test/images', '%s')
+
+        self.ids = list()
+        self.full_ids = list()
+        with open(os.path.join('/Users/ishaghodgaonkar/PyTorch_BlazeFace/', 'WIDER/wider_face_split/img_list.txt'), 'r') as f:
+          self.ids = [(((tuple(line.split('/')))[1]).split('.'))[0] + '.xml' for line in f]
+
+        with open(os.path.join('/Users/ishaghodgaonkar/PyTorch_BlazeFace/', 'WIDER/wider_face_split/img_list.txt'), 'r') as f:
+          self.full_ids = [tuple(line.split()) for line in f]
+
+    def __getitem__(self, index):
+        im, gt, h, w = self.pull_item(index)
+
+        return im, gt
+
+    def __len__(self):
+        return len(self.ids)
+
+    def pull_item(self, index):
+
+        img_id = self.ids[index]
+        full_id = self.full_ids[index]
+        #
+        # print(img_id)
+        # print(full_id)
+        # print(self._annopath % img_id)
+        # print(self._imgpath % (full_id[0].split('.')[0] + '.jpg'))
+        target = ET.parse(self._annopath % img_id).getroot()
+        img = cv2.imread(self._imgpath % (full_id[0].split('.')[0] + '.jpg'))
+        # print(img)
+        height, width, channels = img.shape
+
+        if self.target_transform is not None:
+
+            target = self.target_transform(target, width, height)
+
+        if self.transform is not None:
+            target = np.array(target)
+            img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
+            # to rgb
+            img = img[:, :, (2, 1, 0)]
+            # img = img.transpose(2, 0, 1)
+            target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
+        return torch.from_numpy(img).permute(2, 0, 1), target, height, width
+        # return torch.from_numpy(img), target, height, width
+
+
+    def pull_tensor(self, index):
+            '''Returns the original image at an index in tensor form
+            Note: not using self.__getitem__(), as any transformations passed in
+            could mess up this functionality.
+            Argument:
+                index (int): index of img to show
+            Return:
+                tensorized version of img, squeezed
+            '''
+            return torch.Tensor(self.pull_image(index)).unsqueeze_(0)
+
+    def pull_image(self, index):
+            '''Returns the original image object at index in PIL form
+            Note: not using self.__getitem__(), as any transformations passed in
+            could mess up this functionality.
+            Argument:
+                index (int): index of img to show
+            Return:
+                PIL img
+            '''
+            # print(index)
+            # img_id = self.ids[index]
+            # full_id = self.full_ids[index]
+            # print(self._imgpath % (full_id[0].split('.')[0] + '.jpg'))
+            return cv2.imread(index)
+
+    def pull_anno(self, index):
+            '''Returns the original annotation of image at index
+            Note: not using self.__getitem__(), as any transformations passed in
+            could mess up this functionality.
+            Argument:
+                index (int): index of img to get annotation of
+            Return:
+                list:  [img_id, [(label, bbox coords),...]]
+                    eg: ('001718', [('dog', (96, 13, 438, 332))])
+            '''
+            img_id = self.ids[index]
+            anno = ET.parse(self._annopath % img_id).getroot()
+            gt = self.target_transform(anno, 1, 1)
+            return img_id[1], gt
+
+
+def detection_collate(batch):
+    """Custom collate fn for dealing with batches of images that have a different
+    number of associated object annotations (bounding boxes).
+    Arguments:
+        batch: (tuple) A tuple of tensor images and lists of annotations
+    Return:
+        A tuple containing:
+            1) (tensor) batch of images stacked on their 0 dim
+            2) (list of tensors) annotations for a given image are stacked on 0 dim
+    """
+    targets = []
+    imgs = []
+    for _, sample in enumerate(batch):
+        for _, tup in enumerate(sample):
+            if torch.is_tensor(tup):
+                imgs.append(tup)
+            elif isinstance(tup, type(np.empty(0))):
+                annos = torch.from_numpy(tup).float()
+                targets.append(annos)
+
+    return (torch.stack(imgs, 0), targets)
