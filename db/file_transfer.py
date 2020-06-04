af72501edfe49193d674248663d5f88c3b958353
lee2998p <59533465+lee2998p@users.noreply.github.com>
2020-06-05 01:21:10 +0900

    Update file_transfer.py

--- db/file_transfer.py
+++ db/file_transfer.py
@@ -1,18 +1,17 @@
-from fptlib import fptlib
+from ftplib import FTP
 import os
 import mysql.connector
 import sys
 import config
 
-class FTP_Conn
+class FTP_Conn:
+    
     def __init__(self):
        
-
-
-
-function in class
-
-#image data while opening file
-
-#for/while loop for sending files   condition for the loop
-#close session after that
+        server_connect = ftplib.FTP(host, username, password, account)
+        for x in range(10):         #for/while loop for sending files   condition for the loop
+            file = open(image, 'rb')
+            server_connect.storbinary(STOR image, file)     #will have to make sure that the image is stored to the correct place in the helps machine
+                                                            #Can I use a place holder or an index?
+            file.close()
+        server_connect.quit()
