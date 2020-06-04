be6d1149b3f19aecdc892cccfbea4be71ffac4db
ZPBerg <bergz@purdue.edu>
2020-06-04 15:51:50 -0400

    Update example params

--- jetson/goggles/params.py
+++ jetson/goggles/params.py
@@ -1,9 +1,9 @@
 # hyperparameters for goggleClassifier
 
 hparams = {
-    'lr': 0.001,
+    'lr': 0.0001,
     'momentum': 0.9,
     'step_size': 10,
     'gamma': 0.25,
-    'num_epochs': 5
+    'num_epochs': 50
 }
