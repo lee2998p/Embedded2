6104170a98709972f321dc5dcd25e8ec03bff65f
lee2998p <59533465+lee2998p@users.noreply.github.com>
2020-06-05 02:53:10 +0900

    Update file_transfer.py

--- db/file_transfer.py
+++ db/file_transfer.py
@@ -8,10 +8,29 @@ class FTP_Conn:
     
     def __init__(self):
        
-        server_connect = ftplib.FTP(host, username, password, account)
+        try:
+            server_connect = ftplib.FTP(host, username, password, account)
+        
+        except:
+            print('Error with connecting to the helps machine')
+       
+        pass:
+            exit()
+    
+    def transfer:                       #input: location in jetson nano and location in the helps machine
         for x in range(10):         #for/while loop for sending files   condition for the loop
-            file = open(image, 'rb')
-            server_connect.storbinary(STOR image, file)     #will have to make sure that the image is stored to the correct place in the helps machine
-                                                            #Can I use a place holder or an index?
-            file.close()
-        server_connect.quit()
+            try:
+                file = open(image, 'rb')
+                server_connect.storbinary(STOR image, file)     #will have to make sure that the image is stored to the correct place in the helps machine
+                                                                #Can I use a place holder or an index?
+                file.close()
+                print('File was transferred correctly')
+            except:
+                print('Error with transferring image')
+                pass
+                
+     def disconnect: 
+        try:
+            server_connect.quit()
+        except:
+            print('Error with disconnecting from the helps machine')
