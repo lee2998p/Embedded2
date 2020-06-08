22779c1411306122041d94172367d98e91ead3bf
pawar4 <pawar4@purdue.edu>
2020-06-08 16:17:34 -0400

    Update FTP

--- db/file_transfer.py
+++ db/file_transfer.py
@@ -1,6 +1,5 @@
-from ftplib import FTP
+import paramiko
 import os
-import mysql.connector
 import sys
 import config
 
@@ -9,29 +8,32 @@ class FTPConn:
     def __init__(self):
        
         try:
-            server_connect = FTP(host='ee220clnx1.ecn.purdue.edu',
-                                user='pawar4', 
-                                passwd='PRagya145!')
-
+            self.transport = paramiko.Transport(('', 22))
+            self.transport.connect(username='', password='')
+            self.sftp = paramiko.SFTPClient.from_transport(self.transport)
             print('Sucessfully connected to host machine')
 
         except Exception as e:
             print(e)
             print('Error connecting to the host machine')
-    
-    def transfer(self, input_path, output_path, batch_size):                      
-        for x in range(batch_size):         #for/while loop for sending files   condition for the loop
-            try:
-                file = open(image, 'rb')
-                server_connect.storbinary('STOR' + str(image), file)     #will have to make sure that the image is stored to the correct place in the helps machine
-                                                                #Can I use a place holder or an index?
-                file.close()
-                print('File was transferred correctly')
-            except:
-                print('Error with transferring image')
-                pass
-    def disconnect(self): 
+
+    def transfer(self, input_dir, output_dir):
+
+        if not input_dir.endswith('/'):
+            input_dir = input_dir + '/'
+
+        if not output_dir.endswith('/'):
+            output_dir = output_dir + '/'
+
         try:
-            server_connect.quit()
-        except:
-            print('Error with disconnecting from the helps machine')
+            for image in os.listdir(input_dir):
+                if image.endswith('.py'):
+                    self.sftp.put(input_dir + image, output_dir + image)
+        
+        except Exception as e:
+            print(e)
+    
+    def disconnect(self):
+
+        self.sftp.close()
+        self.transport.close()
--- db/main.py
+++ db/main.py
@@ -1,12 +1,15 @@
-from imagedb import ImageDB
+#from imagedb import ImageDB
 from file_transfer import FTPConn
 
 def main(): 
 
     #dbcon = ImageDB()
-    ftpcon = FTPConn()
     #dbcon.init_tables()
     #dbcon.insert_image('test_image', '6-3-20', '8:46AM','asdfg')
+
+    ftpconn = FTPConn()
+    ftpconn.transfer('.', './Documents')
+    ftpconn.disconnect()
 
 if __name__ == '__main__':
     main()
