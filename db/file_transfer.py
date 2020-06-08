e000904b3f0b335bef2643a7d725f59c731146e0
Akshay Pawar <pawar4@ecegrid-thin4.ecn.purdue.edu>
2020-06-07 21:56:09 -0400

    Update

--- db/file_transfer.py
+++ db/file_transfer.py
@@ -4,24 +4,25 @@ import mysql.connector
 import sys
 import config
 
-class FTP_Conn:
+class FTPConn:
     
     def __init__(self):
        
         try:
-            server_connect = ftplib.FTP(host, username, password, account)
-        
+            server_connect = ftplib.FTP('ee220clnx1.ecn.purdue.edu',
+                                        '', 
+                                        '')
+
+            print('Sucessfully connected to host machine')
+
         except:
-            print('Error with connecting to the helps machine')
-       
-        pass:
-            exit()
+            print('Error with connecting to the host machine')
     
     def transfer(self, input_path, output_path, batch_size):                      
         for x in range(batch_size):         #for/while loop for sending files   condition for the loop
             try:
                 file = open(image, 'rb')
-                server_connect.storbinary(STOR image, file)     #will have to make sure that the image is stored to the correct place in the helps machine
+                server_connect.storbinary('STOR' + str(image), file)     #will have to make sure that the image is stored to the correct place in the helps machine
                                                                 #Can I use a place holder or an index?
                 file.close()
                 print('File was transferred correctly')
--- db/main.py
+++ db/main.py
@@ -1,10 +1,12 @@
 from imagedb import ImageDB
+from file_transfer import FTPConn
 
 def main(): 
 
     dbcon = ImageDB()
-    dbcon.init_tables()
-    dbcon.insert_image('test_image', '6-3-20', '8:46AM','asdfg')
+    ftpcon = FTPConn()
+    #dbcon.init_tables()
+    #dbcon.insert_image('test_image', '6-3-20', '8:46AM','asdfg')
 
 if __name__ == '__main__':
     main()
