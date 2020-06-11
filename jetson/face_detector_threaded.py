ad0a6df70c2d86fd0d8b1144cb4831908604a577
jbagnara <jbagnara@purdue.edu>
2020-06-11 08:15:47 -0500

    Resolved bug in AES import and multiprocessing Exception handler; Improved modularity of code

--- jetson/face_detector_threaded.py
+++ jetson/face_detector_threaded.py
@@ -14,16 +14,15 @@ from BlazeFace_2.blazeface import BlazeFace
 from data import BaseTransform
 from ssd import build_ssd
 
-import sys
-sys.path.append("../Encryption")
-from AES import encryption
+import sys, os, inspect
+
+from AES import encryption as Encryptor
 
 from threading import Thread
 import multiprocessing
 from multiprocessing import Process
 from multiprocessing.managers import SharedMemoryManager
 
-import os
 fileCount = None
 
 class FaceDetector:
@@ -116,48 +115,6 @@ class FaceDetector:
                 bboxes.append((xmin, ymin, xmax, ymax))
             return bboxes
 
-
-def classify(face, classifier):
-    if 0 in face.shape:
-        pass
-    rgb_face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-    pil_face = Image.fromarray(rgb_face)
-
-    # see what the classifier sees
-    #plt.imshow(pil_face)
-    #plt.show()
-
-    transform = transforms.Compose([
-        transforms.Resize(224),
-        transforms.RandomGrayscale(1),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    transformed_face = transform(pil_face)
-    face_batch = transformed_face.unsqueeze(0)
-    device = torch.device("cuda:0" if args.cuda and torch.cuda.is_available() else "cpu")
-    with torch.no_grad():
-        face_batch = face_batch.to(device)
-        labels = classifier(face_batch)
-        m = torch.nn.Softmax(1)
-        softlabels = m(labels)
-        print('Probability labels: {}'.format(softlabels))
-        _, pred = torch.max(labels, 1)
-
-    return pred, softlabels
-
-
-def encrypt(coordinates, encryptedImg): 
-    #Accepts list of face coordinates and img,
-    #Calls AES to encrypt region
-    #For now, returns img with encrypted face(s)
-
-    encryptor = encryption()
-    encryptedImg, _ = encryptor.encrypt(coordinates, encryptedImg)
-
-    return encryptedImg
-
-
 class VideoCapturer(object):
     def __init__(self, src=0):
         self.capture = cv2.VideoCapture(src)
@@ -177,43 +134,51 @@ class VideoCapturer(object):
 
 
 class Classifier:
-    def __init__(self):
+    def __init__(self, classifier):
         self.goggle_probs = []
         self.glasses_probs = []
         self.neither_probs = []
         self.preds = []
         self.fps = 0
+        self.classifier = classifier
  
         global fileCount
         fileCount = 0
 
-    def processFrame(self, img, detector):
+    def classifyFace(self, face):
+        classifier = self.classifier
 
-        #TODO add frame data type for clarity
+        if 0 in face.shape:
+            pass
+        rgb_face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+        pil_face = Image.fromarray(rgb_face)
+    
+        # see what the classifier sees
+        #plt.imshow(pil_face)
+        #plt.show()
+    
+        transform = transforms.Compose([
+            transforms.Resize(224),
+            transforms.RandomGrayscale(1),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        transformed_face = transform(pil_face)
+        face_batch = transformed_face.unsqueeze(0)
+        device = torch.device("cuda:0" if args.cuda and torch.cuda.is_available() else "cpu")
+        with torch.no_grad():
+            face_batch = face_batch.to(device)
+            labels = classifier(face_batch)
+            m = torch.nn.Softmax(1)
+            softlabels = m(labels)
+            print('Probability labels: {}'.format(softlabels))
+            _, pred = torch.max(labels, 1)
 
-        start_time = time.time()
-        boxes = detector.detect(img)
-
-        encryptedImg = img.copy() #copy for creating encrypted image
-
-        p1 = Thread(target=self.encryptFrame, args=(encryptedImg, boxes))
-        p1.start()
-
-        label, softlabels = self.classifyFrame(img, boxes)
-            #if args.encrypt_flag:
-            #    img = encryptedImg #encrypt stream
-
-        self.printProbs()
-
-        self.fps = 1 / (time.time() - start_time)
-        img = cv2.putText(img, 'label: %s' % class_names[label], (30, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                                  (0, 0, 0))
-        img = cv2.putText(img, 'fps: %.3f' % self.fps, (30, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-        cv2.imshow("Face Detect", img)
-
-        p1.join()
+        return pred, softlabels
 
     def classifyFrame(self, img, boxes):
+        label = None
+        softlabels = None
         for box in boxes:
             x1, y1, x2, y2 = [int(b) for b in box]
             # draw boxes within the frame
@@ -228,39 +193,19 @@ class Classifier:
             if args.cropped:
                 height = face.shape[0]
                 face = face[round(0.15 * height):round(0.6 * height), :, :]
-                img = cv2.rectangle(img, (x1, y1 + round(0.15 * height)), (x2, y2 - round(0.4 * height)),
-    								(0, 255, 0), 2)
+                img = cv2.rectangle(img, 
+                        (x1, y1 + round(0.15 * height)), 
+                        (x2, y2 - round(0.4 * height)),
+                        (0, 255, 0), 2)
     
-            label, softlabels = classify(face, g)
+            label, softlabels = self.classifyFace(face)
     
             self.glasses_probs.append(softlabels[0][0].item())
             self.goggle_probs.append(softlabels[0][1].item())
             self.neither_probs.append(softlabels[0][2].item())
             self.preds.append(label.item())
 
-            return label, softlabels
-
-    def encryptFrame(self, img, boxes):
-        for box in boxes:
-            x1, y1, x2, y2 = [int(b) for b in box]
-            # draw boxes within the frame
-            x1 = max(0, x1)
-            y1 = max(0, y1)
-            x2 = min(img.shape[1], x2)
-            y2 = min(img.shape[0], y2)
-
-            encrypt([(x1, y1, x2, y2)], img)
-
-        #TODO ftp img to remote
-        #Lets just write img for now
-        global fileCount
-
-        face_file_name = os.path.join("out/", f'{fileCount}.jpg')
-
-        fileCount += 1
-
-        cv2.imwrite(face_file_name, img)
-            
+        return label, softlabels
 
     def printProbs(self):
         goggle_probs = self.goggle_probs
@@ -291,6 +236,41 @@ class Classifier:
             print('\n')
 
 
+def encryptFace(coordinates, img): 
+    #Accepts list of face coordinates and img,
+    #Calls AES to encrypt region
+    #For now, returns img with encrypted face(s)
+
+    encryptor = Encryptor()
+    encryptedImg, _ = encryptor.encrypt(coordinates, img)
+
+    return encryptedImg
+
+def encryptFrame(img, boxes):
+    try:
+        for box in boxes:
+            x1, y1, x2, y2 = [int(b) for b in box]
+            # draw boxes within the frame
+            x1 = max(0, x1)
+            y1 = max(0, y1)
+            x2 = min(img.shape[1], x2)
+            y2 = min(img.shape[0], y2)
+    
+            img = encryptFace([(x1, y1, x2, y2)], img)
+    
+        #TODO ftp img to remote
+        #Lets just write img for now
+        global fileCount
+        #if fileCount <= 50:
+        face_file_name = os.path.join(args.output_dir, f'{fileCount}.jpg')
+    
+        print("writing ", face_file_name)
+        fileCount += 1
+        cv2.imwrite(face_file_name, img) 
+    except KeyboardInterrupt:
+        pass
+
+
 if __name__ == "__main__":
     warnings.filterwarnings("once")
     parser = argparse.ArgumentParser(description="Face detection")
@@ -299,28 +279,60 @@ if __name__ == "__main__":
     parser.add_argument('--classifier', type=str, help="Path to a trained classifier .pth file")
     parser.add_argument('--cropped', default=False, action='store_true',
                         help="Crop out half the face? Make sure your model is trained on cropped images")
-    parser.add_argument('--encrypt_flag', default=False)
+    parser.add_argument('--output_dir', default='encrypted_imgs', type=str, help="Where to output encrypted images")
     args = parser.parse_args()
-    detector = FaceDetector(trained_model=args.trained_model, cuda=args.cuda and torch.cuda.is_available(),
-                            set_default_dev=True)
-
-    cap = VideoCapturer()
-    cl = Classifier()
 
     device = torch.device('cpu')
     if args.cuda and torch.cuda.is_available():
         device = torch.device('cuda:0')
 
+    if not os.path.isdir(args.output_dir):
+        os.mkdir(args.output_dir)
+
     g = torch.load(args.classifier, map_location=device)
     g.eval()
     class_names = ['Glasses', 'Goggles', 'Neither']
 
+    cap = VideoCapturer() #Instantiate Video Capturer object
+    detector = FaceDetector(trained_model=args.trained_model, cuda=args.cuda and torch.cuda.is_available(), set_default_dev=True) #Instantiate Face Detector object
+    cl = Classifier(g) #Instantiate Classifier object
+
     while True:
+        start_time = time.time()
+
         frame = cap.get_frame()
-        cl.processFrame(frame, detector)
-        
+        boxes = detector.detect(frame)
+
+        encryptedImg = frame.copy() #copy for creating encrypted image
+
+        if len(boxes) != 0:
+            p1 = Thread(target=encryptFrame, args=(encryptedImg, boxes))
+            p1.daemon = True
+            p1.start()
+
+        label, softlabels = cl.classifyFrame(frame, boxes)
+        cl.printProbs()
+
+        fps = 1 / (time.time() - start_time)
+        if len(boxes) != 0:
+            frame = cv2.putText(frame, 
+                    'label: %s' % class_names[label], 
+                    (30, 120), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (0, 0, 0))
+
+        frame = cv2.putText(frame, 
+                'fps: %.3f' % fps, 
+                (30, 100), 
+                cv2.FONT_HERSHEY_SIMPLEX, 
+                0.5, (0, 0, 0))
+        cv2.imshow("Face Detect", frame)
+
+        p1.join()
+
         if cv2.waitKey(1) == 27:
            break
+
     cv2.destroyAllWindows()
     exit(0)
     #else:
