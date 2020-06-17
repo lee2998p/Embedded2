48ab1ab23fdb992a165a7782a9cf88aaf4e7d3e2
pawar4 <pawar4@purdue.edu>
2020-06-17 17:13:46 -0400

    Updated db_connection and file_transfer functions

--- jetson/db/db_connection.py
+++ jetson/db/db_connection.py
@@ -1,130 +1,59 @@
 import mysql.connector
-from mysql.connector import errorcode
-
-import sys
 import config
-
 from contextlib import contextmanager, closing
 
 
-#class SQLConn:
+class Table:
+    def __init__(self):
+        pass
 
-    # def __init__(self):
-    #     """setup connection to MySQL database
-    #     """
 
-    #     #Specify database
-    #     mydatabase = config.KEYSPACE
+class IMAGE(Table):
+    """Class defines the Image table in the database with init inputs being the columns in the table.
+    Class name must match exactly the spelling of the corresponding table in database
 
-    #     try:
-    #         self.mydb = mysql.connector.connect(
-    #             host=config.SQL_HOST,
-    #             user=config.USER_NAME,
-    #             password=config.PASSWORD,
-    #             database= mydatabase
-    #         )
-    #         print('Connected to mysql databse' + mydatabase)
+    To insert into database table using this class, use this class as input for function sql_insert:
+        sql_insert(IMAGE('image_name', 'image_date', 'image_time', 'init_vector'))
 
-    #     except mysql.connector.Error as err:
-    #         print(str(err))
-    #         sys.exit()
-    #     except _mysql_connector.MySQLInterfaceError as e:
-    #         print(str(e))
-    #         sys.exit()
-    #     except Exception as e:
-    #         print(str(e))
-    #         sys.exit()
+        Args:
+            image_name (string) : name of image
+            image_date (datetime obj) : date image was taken
+            image_time (datetime obj) : time image was taken
+            init_vector (string) : decryption key for encrypted image
+    """
 
-    #     self.mycursor = self.mydb.cursor(buffered=True)
+    def __init__(self, image_name, image_date, image_time, init_vector):
+        self.Image_Name = image_name
+        self.Image_Date = image_date
+        self.Image_Time = image_time
+        self.Init_Vector = init_vector
 
-    # def dropImageTable(self):
-    #     """Delete image table
-    #     """
-    #     self.mycursor.excute('DROP TABLE IF EXISTS IMAGE')
-    #     print('IMAGE table dropped')
 
-    # def dropBBoxTable(self):
-    #     """Delete bounding box table
-    #     """
-    #     self.mycursor.execute('DROP TABLE IF EXISTS BBOX')
-    #     print('BBOX table dropped')
+class BBOX(Table):
+    """Class defines the BBox table in the database with init inputs being the columns in the table.
+    Class name must match exactly the spelling of the corresponding table in database
 
-    # def CreateImageTable(self):
-    #     """Create image table
-    #     """
-    #     try:
-    #         self.mycursor.execute('SELECT 1 FROM IMAGE LIMIT 1')
-    #         print('IMAGE table exists')
+    To insert into database table using this class, use this class as input for function sql_insert:
+        sql_insert(BBOX(xmin, ymin, xmax, ymax, conf, goggles))
 
-    #     except:
-    #         self.mycursor.execute('CREATE TABLE IMAGE(Image_Name VARCHAR(25) NOT NULL,\
-    #                                 Image_Date DATE NOT NULL,\
-    #                                 Image_Time TIME NOT NULL,\
-    #                                 Init_Vector VARCHAR(25) NOT NULL,\
-    #                                     PRIMARY KEY(Image_Name))')
-    #         print('IMAGE table created')
+        Args:
+            xmin (float) : lower left x-coordinate of bounding box
+            ymin (float) : lower left y-coordinate of bounding box
+            xmax (float) : upper right x-coordinate of bouding box
+            ymax (float) : upper right y-coordinate of bounding box
+            conf (float) : confidence score for bounding box
+            goggles (bool) : whether goggles were detected in bounding box
+            image_name (string): name of image that contains the bounding box
+    """
 
-    # def CreateBBoxTable(self):
-    #     """Create bounding box table
-    #     """
-    #     try:
-    #         self.mycursor.execute('SELECT 1 FROM BBOX LIMIT 1')
-    #         print('BBOX table exists')
-
-    #     except:
-    #         self.mycursor.execute('CREATE TABLE BBOX(X_Min VARCHAR(10) NOT NULL,\
-    #                                 Y_Min VARCHAR(10) NOT NULL,\
-    #                                 X_Max VARCHAR(10) NOT NULL,\
-    #                                 Y_Max VARCHAR(10) NOT NULL,\
-    #                                 Confidence VARCHAR(10) NOT NULL,\
-    #                                 Goggles VARCHAR(10) NOT NULL,\
-    #                                 Image_Name VARCHAR(25) NOT NULL,\
-    #                                     PRIMARY KEY(X_Min, Y_Min, X_Max, Y_Max, Image_Name))')
-    #         print('IMAGE table created')
-
-    # def insertImage(self, image):
-    #     """Insert image metadata into image table
-
-    #     Arguments:
-    #         image {tuple} -- tuple containing the 4 variables of image metadata
-    #     """
-    #     sql = 'INSERT INTO IMAGE(Image_Name, Image_Date, Image_Time, Init_Vector) \
-    #                             VALUES (%s, %s, %s, %s), \
-    #                             ON DUPLICATE KEY UPDATE \
-    #                             Image_Name=VALUES(image_name), Image_Date=VALUES(image_date),\
-    #                             Image_Time=VALUES(image_time), Init_Vector=VALUES(init_vector)'
-
-    #     self.mycursor.execute(sql, image)
-
-    # def insertBBox(self, bbox):
-    #     """Insert bounding box information into bounding box table
-
-    #     Arguments:
-    #         bbox {tuple} -- tuple containing 7 variables of bounding box information
-    #     """
-    #     sql = 'INSERT INTO BBOX(X_Min, Y_Min, X_Max, Y_Max, Confidence, Goggles, Image_Name) \
-    #                             VALUES (%s, %s, %s, %s, %s, %s, %s), \
-    #                             ON DUPLICATE KEY UPDATE \
-    #                             X_Min=VALUES(x_min), Y_Min=VALUES(y_min), \
-    #                             X_Max=VALUES(x_max), Y_Max=VALUES(y_max), \
-    #                             Confidence=VALUES(confidence), Goggles=VALUES(goggles), \
-    #                             Image_Name=VALUES(image_name)'
-
-    #     self.mycursor.execute(sql, bbox)
-
-    # def clearTable(self, table_name):
-    #     """Clear input table in the MySQL database
-
-    #     Arguments:
-    #         table_name {string} -- string containing table name that needs to be cleared
-    #     """
-    #     try:
-    #         sql = 'DELETE FROM' + table_name
-    #         self.mycursor.execute(sql)
-    #         self.mydb.commit()
-    #     except Exception as e:
-    #         print(e)
-    #         sys.exit()
+    def __init__(self, xmin, ymin, xmax, ymax, conf, goggles, image_name):
+        self.X_Min = xmin
+        self.Y_Min = ymin
+        self.X_Max = xmax
+        self.Y_Max = ymax
+        self.Confidence = conf
+        self.Goggles = goggles
+        self.Image_Name = image_name
 
 
 @contextmanager
@@ -132,7 +61,7 @@ def sql_connection():
     """Sets up connection to mysql database
 
     Yields:
-        sql connection: sql connector object to the sql database 
+        sql connection: sql connector object to the sql database
     """
     connection = mysql.connector.connect(
         host=config.SQL_HOST,
@@ -153,26 +82,27 @@ def sql_cursor():
        sql cursor : cursor object to database
     """
     with sql_connection() as connection:
-        yield connection.cursor()
+        yield connection.cursor(buffered=True)
+        connection.commit()
 
 
-def sql_insert(table_name, **kwargs):
+def sql_insert(table: Table):
     """Inserts row of information for a specified table in database
 
     Args:
-        table_name (string): name of table in database, must be spelt exactly as specified in database
+        table (class obj): Class with name that corresponds to the table for data to be inserted into,
+                            class name must match the database table name exactly
     """
-    key_list = ','.join([key for key, _ in kwargs.items()])
-    value_list = ','.join([f'%({key})s' for key, _ in kwargs.items()])
-    query = f"INSERT INTO %(table_name)s ({key_list}) VALUES({value_list})"
-    params = kwargs
-
-    params['table_name'] = table_name
+    key_list = ','.join([key for key, _ in table.__dict__.items()])
+    value_list = ','.join([f'%({key})s' for key, _ in table.__dict__.items()])
+    query = f"INSERT INTO {table.__class__.__name__}({key_list}) VALUES({value_list})"
+    params = table.__dict__
     with sql_cursor() as cursor:
         try:
             cursor.execute(query, params)
         except Exception as e:
             print(e)
+
 
 def sql_clear_table(table_name):
     """Clears all rows in specified table in the database
@@ -180,39 +110,16 @@ def sql_clear_table(table_name):
     Args:
         table_name (string): name of table in database, must be spelt exactly as specified in database
     """
-    query = 'DELETE FROM %(table_name)s'
-    params = {'table_name': table_name}
+    query = f"DELETE FROM {table_name}"
+    
     with sql_cursor() as cursor:
-        cursor.execute(query, params)
-
-def insert_image(image_name, image_date, image_time, init_vector):
-    """Inserts image metadata into IMAGE table in database
-
-    Args:
-        image_name (string): name of image
-        image_date (datetime obj): date the image was taken
-        image_time (datatime obj): time the image was taken
-        init_vector (string): decryption string for encrypted image
-    """
-    sql_insert('IMAGE', Image_Name=image_name, Image_Date=image_date,
-               Image_Time=image_time, Init_Vector=init_vector)
-
-
-def insert_bbox(x_min, y_min, x_max, y_max, confidence, goggles, image_name):
-    """Inserts bounding boxes for an images into BBOX table in database
-
-    Args:
-        x_min (float): bottom left x-coordinate for bounding box
-        y_min (float): bottom left y-coordinate for bounding box
-        x_max (float): top right x-coordinate for bounding box
-        y_max (float): top right y-coordinate for bounding box
-        confidence (float): confidence score for bounding box
-        goggles (bool): whether goggles were detected in the bounding box
-        image_name (string): name of image that contains the bounding box
-    """
-    sql_insert('BBOX', X_Min=x_min, Y_Min=y_min, X_Max=x_max,
-               Y_Max=y_max, Confidence=confidence, Goggles=goggles, Image_Name=image_name)
+        try:
+            cursor.execute(query)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
-    insert_image('testimage', '100', '123', '232141fs')
+
+    sql_clear_table('IMAGE')
+    sql_insert(IMAGE('testia1ge', '100', '123', '232141fs'))
--- jetson/db/file_transfer.py
+++ jetson/db/file_transfer.py
@@ -4,53 +4,15 @@ import sys
 import config
 from contextlib import contextmanager
 
-# class FTPConn:
-    
-#     def __init__(self):
-#        #setup connection with host machine
-#         try:
-#             self.transport = paramiko.Transport((config.FTPHOST, 22))
-#             self.transport.connect(username=config.FTPUSER, password=config.FTPPASS)
-#             self.sftp = paramiko.SFTPClient.from_transport(self.transport)
-#             print('Sucessfully connected to host machine')
-
-#         except Exception as e:
-#             print(e)
-
-#     def transfer(self, input_dir, output_dir, image_name):
-#         #ensure correct format for directory paths
-#         if not input_dir.endswith('/'):
-#             input_dir = input_dir + '/'
-#         if not output_dir.endswith('/'):
-#             output_dir = output_dir + '/'
-
-#         #transfer files from input directory to output directory
-#         try:
-#             #only transfer files that end with certain type(.jpg for testing purposes)
-#             if input_dir.endswith('.jpg'):
-#                 self.sftp.put(input_dir, output_dir + image_name)
-#                 #os.remove(input_dir + image) #remove the image from client side after successful transfer
-    
-#             return True
-        
-#         except Exception as e:
-#             print(e)
-#             return False
-    
-#     def disconnect(self):
-        
-#         #disconnect sftp session
-#         self.sftp.close()
-#         self.transport.close()
 
 @contextmanager
 def ftp_transfer():
     """Sets up connection to target machine using SFTP and returns transfer function
-        
+
     Example Usage:
         with ftp_transfer() as transfer:
             transfer("./input_dir", "./output_dir", "file_name.ext")
-        
+
     Yields:
         [function]: transfers file to output dir using SFTP
 
@@ -68,31 +30,32 @@ def ftp_transfer():
         print('Sucessfully connected to host machine')
 
         def transfer(input_dir, output_dir, image_name):
-            #ensure correct format for directory paths
-            print("being used")
+            # ensure correct format for directory paths
             if not output_dir.endswith('/'):
                 output_dir = output_dir + '/'
 
-            
-            #transfer files from input directory to output directory
+            # transfer files from input directory to output directory
             try:
-                #only transfer files that end with certain type(.jpg for testing purposes)
+                # only transfer files that end with certain type(.jpg for testing purposes)
                 if input_dir.endswith('.py'):
                     sftp.put(input_dir, output_dir + image_name)
-                    #os.remove(input_dir + image) #remove the image from client side after successful transfer
-
+                    # remove the image from client side after successful transfer
+                    # os.remove(input_dir)
+                    
                 return True
-            
             except Exception as e:
                 print(e)
                 return False
-    
         yield transfer
 
     except Exception as e:
         print(e)
         sys.exit()
-    
     finally:
         sftp.close()
         transport.close()
+
+
+if __name__ == '__main__':
+    with ftp_transfer() as transfer:
+        transfer('./main.py', './Documents', 'main.py')
