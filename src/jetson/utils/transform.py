b04602abdf0408dec8ba15b11c723c571afb6a0d
Aditya Chakraborty <chakra17@purdue.edu>
2020-06-19 15:47:44 -0400

    Moved documentation inside constructor

--- src/jetson/utils/transform.py
+++ src/jetson/utils/transform.py
@@ -3,25 +3,29 @@ import numpy as np
 
 
 class BaseTransform:
-    '''
-    Transform image to desired size before passing to face detector
-
-    Params:
-    size (Type: int) - Desired input size to face detector
-    mean (Type: tuple of length 3 for 3 channels) - mean intensity values to perform normalization.
-
-    '''
-
     def __init__(self, size, mean):
+        '''
+        Transform image to desired size before passing to face detector
+
+        Params:
+        size (Type: int) - Desired input size to face detector
+        mean (Type: tuple of length 3 for 3 channels) - mean intensity values to perform normalization.
+
+        '''
+
         self.size = size
-        self.mean = np.array(mean, dtype=np.float32)
+        if mean is not None:
+            self.mean = np.array(mean, dtype=np.float32)
+        else:
+            self.mean = None
 
     def __call__(self, image, boxes=None, labels=None):
-        return self.base_transform(image, self.size, self.mean), boxes, labels
+        return self.base_transform(image), boxes, labels
 
 
-    def base_transform(self, image, size, mean):
-        x = cv2.resize(image, (size, size)).astype(np.float32)
-        x -= mean
+    def base_transform(self, image):
+        x = cv2.resize(image, (self.size, self.size)).astype(np.float32)
+        if self.mean is not None:
+            x -= self.mean
         x = x.astype(np.float32)
         return x
