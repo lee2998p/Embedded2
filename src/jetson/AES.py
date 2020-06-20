b19d9a80658793acb3db22a0ed7bbceab77d3b06
Aditya Chakraborty <chakra17@purdue.edu>
2020-06-20 14:35:25 -0400

    Added documentation for AES encryption

--- src/jetson/AES.py
+++ src/jetson/AES.py
@@ -8,23 +8,39 @@ import os
 THRES = 400
 
 class encryption():
-    
+
     def __init__(self):
-        self.salt = os.urandom(16) #Salt variable
+        '''
+        This class handles encryption to prevent identifiable information (facial data)
+        from leaving the camera. It also generates a random key that will be used by
+        authorized personnel to acces the data.
+        '''
+        self.salt = os.urandom(16) #Salt variable (Generates a random byte string)
         self.key = PBKDF2("passphrase", self.salt).read(16) #Creates key using KDF scheme
-        
+
     def encrypt(self, coordinates, image):
+        '''
+        This method encrypts the facial regions.
+
+        Params-
+        coordinates: bounding box coordinates
+        image: Image to be encrypted
+
+        Returns-
+        image: Encrypted image
+        IV: Initialization vector for decrypting image
+        '''
         IV = os.urandom(16) #Initialization vector
         mode = AES.MODE_CFB #Sets encryption mode to CFB mode; CFB is great in avoiding the hassle of padding
         encryptor = AES.new(self.key, mode, IV) #Encryptor
-        
+
         ROI_number = 0
         for c in coordinates:
             x1,y1,x2,y2 = c
             x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
             ROI = image[y1:y2, x1:x2]
             img_bytes = np.ascontiguousarray(ROI, dtype=None)
-        
+
             encData = encryptor.encrypt(img_bytes) #Encrypted data
             data = np.frombuffer(encData, dtype=np.uint8)
             data = np.reshape(data, (y2-y1, x2-x1, 3))
@@ -33,13 +49,24 @@ class encryption():
 
 
     def decrypt(self, coordinates, image, IV):
+        '''
+        This method decrypts the facial regions.
+
+        Params-
+        coordinates: bounding box coordinates
+        image: Image to be decrypted
+        IV - Initialization vector
+
+        Returns-
+        image: Decrypted image
+        '''
         mode = AES.MODE_CFB #Sets encryption mode to CFB mode; CFB is great in avoiding the hassle of padding
         decryptor = AES.new(self.key, mode, IV) #Decryptor
-        
+
         for c in coordinates:
             x1,y1,x2,y2 = c
             x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
-            
+
             ROI = image[y1:y2, x1:x2]
             img_bytes = np.ascontiguousarray(ROI, dtype=None)
 
@@ -47,5 +74,5 @@ class encryption():
             data = np.frombuffer(decData, dtype=np.uint8)
             data = np.reshape(data, (y2-y1, x2-x1, 3))
             image[y1:y2, x1:x2] = data
-            
+
         return image
