b0c1479ec477f419abe5598aea83fa46204296bb
Aditya Chakraborty <chakra17@purdue.edu>
2020-06-20 19:02:53 -0400

    Added documentation

--- src/jetson/models/SSD/layers/modules/l2norm.py
+++ src/jetson/models/SSD/layers/modules/l2norm.py
@@ -5,6 +5,10 @@ from torch.autograd import Variable
 import torch.nn.init as init
 
 class L2Norm(nn.Module):
+    '''L2 Normalization calculates the distance of the vector coordinate from the origin
+    of vector space
+    It is calculated as the square root of the sum of the squared vector values
+    '''
     def __init__(self,n_channels, scale):
         super(L2Norm,self).__init__()
         self.n_channels = n_channels
@@ -18,7 +22,6 @@ class L2Norm(nn.Module):
 
     def forward(self, x):
         norm = x.pow(2).sum(dim=1, keepdim=True).sqrt()+self.eps
-        #x /= norm
         x = torch.div(x,norm)
         out = self.weight.unsqueeze(0).unsqueeze(2).unsqueeze(3).expand_as(x) * x
         return out
