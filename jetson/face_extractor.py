0c60cb419a9f0ed37f7e30139c407220cb74c1cf
ZPBerg <bergz@purdue.edu>
2020-06-20 17:21:31 -0400

    Clean and document face_extractor

--- jetson/face_extractor.py
+++ jetson/face_extractor.py
@@ -1,56 +1,31 @@
 import argparse
 from glob import glob
-import hashlib
 import os
 import warnings
 
 import cv2
 import numpy as np
 import torch
-from PIL import Image
 from tqdm import tqdm
 
-from face_detector import FaceDetector
+from face_detector_threaded import FaceDetector
 
 warnings.filterwarnings('once')
 
 
-# for videos, saves filenames and hashes
-# for images, saves filenames
-def get_files(input_dir, hash_file=None, images=False):
-    if images:
-        img_ext = ['.jpg', '.JPG', '.png', '.PNG']
-        imgs = [glob(f"{input_dir}/*{e}", recursive=args.recursive) for e in img_ext]
-        images = [img for subimg in imgs for img in subimg]
-        return images
-    else:
-        video_ext = ['.mp4', '.mov', '.MOV', '.MP4']
-        videos = [glob(f"{input_dir}/*{e}", recursive=args.recursive) for e in video_ext]
-        # Reduce the 2d list from before to a 1d list
-        videos = [vid for subvid in videos for vid in subvid]
+def get_files(input_dir, images):
+    """Get filenames from input directory."""
+    file_ext = ['.jpg', '.JPG', '.png', '.PNG'] if images else ['.mp4', '.MP4', '.mov', '.MOV']
+    files = [glob(f"{input_dir}/*{e}") for e in file_ext]
+    # reduce the 2d list from before to a 1d list TODO not sure why
+    files = [file for f in files for file in f]
 
-        hashes = []
-        for video in videos:
-            hasher = hashlib.md5()
-            buf_size = 65536
-            with open(video, 'rb') as video_file:
-                buf = video_file.read(buf_size)
-                while len(buf) > 0:
-                    hasher.update(buf)
-                    buf = video_file.read(buf_size)
-            hashes.append(hasher.hexdigest())
-
-        if hash_file is None:
-            return list(zip(videos, hashes))
-        else:
-            with open(hash_file, 'r') as f:
-                processed_hashes = [line.strip() for line in f]
-            # Filter out the videos that have already been processed
-            return [(file, file_hash) for (file, file_hash) in zip(videos, hashes) if file_hash not in processed_hashes]
+    return files
 
 
 # run the frame through FaceDetector and save the face region
 def save_cropped_img(frame, file_num):
+    """Run frame through FaceDetector and save the cropped face image."""
     if frame is not None and not 0:
         boxes = face_detector.detect(frame)
         for box in boxes:
@@ -65,18 +40,17 @@ def save_cropped_img(frame, file_num):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Extract faces in data to label at a later time using a neural net trained to detect faces")
-    parser.add_argument("--input_dir", default="videos", type=str, help="The directory holding the video")
-    parser.add_argument('-R', '--recursive', default=False, type=bool, help="Search the input dir recursively")
-    parser.add_argument('--output_dir', default='face_imgs', type=str, help="Where to output the extracted faces")
-    parser.add_argument('--hash_file', default=None,
-                        help='File containing md5 hashes of videos that have been processed')
-    parser.add_argument('--trained_model', default='ssd300_WIDER_100455.pth', type=str, help="Trained state_dict file")
-    parser.add_argument('--rate', default=5, type=int, help="Run the network on 1/rate frames in the video")
+        description="Extract faces from images or videos.")
+    parser.add_argument("--input_dir", default="videos", type=str, help="The directory containing the videos/images.")
+    parser.add_argument('--output_dir', default='face_imgs', type=str, help="Output directory for the extracted faces.")
+    parser.add_argument('--trained_model', default='blazeface.pth', type=str, help="Face detector model.")
+    parser.add_argument('--rate', default=5, type=int, help="Run the network on 1/rate frames in the video.")
     parser.add_argument('--cuda', default=False, action='store_true',
-                        help='Run the neural network with cuda enabled (will be disabled if cuda isn\'t available')
+                        help='Use CUDA.')
+    parser.add_argument('--horiz', default=False, action='store_true', help='Rotate the video. If your output images '
+                                                                            'are all upside-down, enable this.')
     parser.add_argument('--images', default=False, action='store_true',
-                        help='Run images through the neural net instead of videos')
+                        help='Extract from images instead of videos.')
     args = parser.parse_args()
 
     if args.cuda and torch.cuda.is_available():
@@ -88,9 +62,7 @@ if __name__ == "__main__":
 
     face_detector = FaceDetector(trained_model=args.trained_model)
 
-    # This lets us break the generation up into different sessions if we want
-    print("Finding files")
-    files_and_hashes = get_files(args.input_dir, args.hash_file, args.images)
+    filenames = get_files(args.input_dir, args.images)
     print("Processing files")
 
     if not os.path.isdir(args.output_dir):
@@ -99,31 +71,26 @@ if __name__ == "__main__":
     file_num = 0
 
     if args.images:
-        for i in files_and_hashes:
+        for i in filenames:
             image = cv2.imread(i)
             save_cropped_img(np.float32(image), file_num)
             file_num += 1
     else:
-        hash_file = args.hash_file
-        if hash_file is None:
-            hash_file = "processed_hashes.txt"
-        print(f"FILES: {files_and_hashes}")
-        with open(hash_file, "w+") as hash_out:
-            for video_file, file_hash in files_and_hashes:
-                print(f"Opening {video_file} :: {file_hash}")
-                video = cv2.VideoCapture(video_file)
-                file_len = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        for video_file in filenames:
+            print(f"Opening {video_file}")
+            video = cv2.VideoCapture(video_file)
+            file_len = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
-                for frame_num in tqdm(range(file_len)):
-                    ret, frame = video.read()
+            for frame_num in tqdm(range(file_len)):
+                ret, frame = video.read()
 
-                    if frame_num % args.rate == 0:
-                        # If the video is shot horizontally, flip it so it's in the right orientation
-                        # frame = cv2.flip(cv2.transpose(frame), 1)
-                        save_cropped_img(frame, file_num)
-                        file_num += 1
+                if frame_num % args.rate == 0:
+                    # If the video is shot horizontally, flip it so it's in the right orientation
+                    if args.horiz:
+                        frame = cv2.transpose(frame)
+                    save_cropped_img(frame, file_num)
+                    file_num += 1
 
-                video.release()
-                hash_out.write(f"{file_hash}\n")
+            video.release()
 
     exit(0)
