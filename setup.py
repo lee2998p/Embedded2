5b6ada8c8d3c8f7b66d4b3918f90a362c8a66746
Stephen Davis <davi1138@purdue.edu>
2020-07-22 21:06:22 -0400

    Fixed travis

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='Embedded2',
+   version='0.5.0',
+   description='Fancy goggle detector',
+   author='Stephen Davis',
+   author_email='hackerman@purdue.edu',
+   packages=['src'],  #same as name
+)
