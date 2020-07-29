b2809f76e7c48007bcf96a56a8277d3832e21bab
jbagnara <bagnarajason@gmail.com>
2020-07-28 21:59:16 -0400

    Fix torch dependency and install issues in setup.py

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#This script is intended to be run on a freshly flashed jetson-nano with jetpack 4.4 to
+#install dependencies used by the Embedded2 project. No other dependencies should need
+#to be resolved to run the 'src/main.py' script aside from configuring .json files
+
 sudo apt-get update -y
 sudo apt-get install -y python3-pip  python3-flask python3-scipy python3-matplotlib python3-paramiko
 
@@ -7,16 +11,17 @@ cd jetson-fan-ctl
 ./install.sh
 cd ../
 
-#PyTorch 1.4.0
-whl="torch-1.4.0-cp36-cp36m-linux_aarch64.whl"
-wget https://nvidia.box.com/shared/static/c3d7vm4gcs9m728j6o5vjay2jdedqb55.whl -O ${whl}
-sudo apt-get install libopenblas-base libopenpi-dev
+#PyTorch 1.6.0
+whl="torch-1.6.0-cp36-cp36m-linux_aarch64.whl"
+wget https://nvidia.box.com/shared/static/yr6sjswn25z7oankw8zy1roow9cy5ur1.whl -O ${whl}
+sudo apt-get install libopenblas-base libopenmpi-dev
+pip3 install Cython
 pip3 install future ${whl} 
 rm ${whl}
 
-#Torchvision v0.5.0
+#Torchvision v0.7.0
 sudo apt-get install libjpeg-dev zlib1g-dev
-git clone --branch "v0.5.0" https://github.com/pytorch/vision torchvision
+git clone --branch "v0.7.0-rc2" https://github.com/pytorch/vision torchvision
 cd torchvision
 sudo python3 setup.py install
 cd ../
