0ed6a4a031e99664da31b95429c24dc680acb7cb
Akshay Pawar <42657031+pawar4@users.noreply.github.com>
2020-08-04 04:52:30 -0400

    Update data_insertion.py

--- src/db/data_insertion.py
+++ src/db/data_insertion.py
@@ -3,11 +3,6 @@ from src.db.db_connection import sql_insert, IMAGE, BBOX
 from decimal import Decimal
 import datetime
 
-"""Only neither and goggles classifications are currently stored in the database as a bit value. Hence these variables define
-the standard being used.
-"""
-GOGGLES = 1
-NEITHER = 0
 
 def data_insert(image_name: str, image_date: datetime, image_time: datetime, init_vecs: list, bboxes: list, input_dir: str, labels: list):
     """Transfer image to remote storage then inserts image metadata and bounding boxes data in database
@@ -18,15 +13,9 @@ def data_insert(image_name: str, image_date: datetime, image_time: datetime, ini
         image_time (datetime obj): time image was taken
         init_vecs (list): list of decryption keys(strings) for encrypted bounding boxes in image
         bboxes (list): list of bounding boxes, each bounding box containing coordinates, confidence and classification
-        labels (list): defines the classifications for each bounding box
+        labels (list): defines the classification for each bounding box
         input_dir (string): image path in client machine
     """
-    
-    """The 3 classifications included in labels are represented as follows:
-    glasses : 0, goggles : 1, neither : 2
-    Glasses classification is currently not being stored in the database so the list is modified before database insertion
-    """
-    labels = [GOGGLES if label == 1 else NEITHER for label in labels]
 
     # Below ftp transfer has been commented out for testing purposes and will be fixed by another team member
     with ftp_transfer() as transfer:
