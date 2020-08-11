67e7b7c1ea47a6fe272199ba82850e07e6692522
lee2998p <59533465+lee2998p@users.noreply.github.com>
2020-08-12 00:02:02 +0900

    Update and rename test_encryptor to test_encryptor.py

---
+++ tests/test_encryptor.py
@@ -0,0 +1,11 @@
+def test_init():
+  assert test_encryptor = AESEncryptor()
+  assert __init__(self).encryptor = test_encryptor
+  
+  assert test_key = self.encryptor.key
+  assert __init__(self).key = test_key
+def test_encryptFace():
+  #Test initialization and returning variable
+def test_encryptFrame():
+  #Test initialization and returning variable
+  #Test the loop
