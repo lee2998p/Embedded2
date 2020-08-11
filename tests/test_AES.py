2fa8d4c49eb1c8c410ba8cac2d05209a7f80c039
lee2998p <59533465+lee2998p@users.noreply.github.com>
2020-08-12 00:03:57 +0900

    Update test_AES.py

--- tests/test_AES.py
+++ tests/test_AES.py
@@ -10,7 +10,7 @@ def test_init():
   assert test_salt = os.urandom(16)
   assert __init__(self).salt = test_salt
   
-  asssert test_key = PBKDF2("passphrase", self.salt).read(16)
+  assert test_key = PBKDF2("passphrase", self.salt).read(16)
   assert __init__(self).key = test_key
   
 def test_encrypt():
